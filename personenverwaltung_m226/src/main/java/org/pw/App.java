4c63330782d3709f49288b55732fb149b3da5374
joshuaodermatt <54634223+joshuaodermatt@users.noreply.github.com>
2020-10-12 19:11:01 +0200

    added tests to setters

--- personenverwaltung_m226/src/main/java/org/pw/App.java
+++ personenverwaltung_m226/src/main/java/org/pw/App.java
@@ -1,13 +1,8 @@
 package org.pw;
 
-import com.apple.eawt.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import org.graalvm.compiler.phases.common.NodeCounterPhase;
 
 public class App {
-
+    /*
     @Override
     public void start(NodeCounterPhase.Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
@@ -15,6 +10,8 @@ public class App {
         primaryStage.setResizable(false);
         primaryStage.show();
     }
+
+     */
 
     public static void main (String[] args ) {
 
--- personenverwaltung_m226/src/test/java/org/pw/PersonTest.java
+++ personenverwaltung_m226/src/test/java/org/pw/PersonTest.java
@@ -4,17 +4,55 @@ import junit.framework.TestCase;
 
 public class PersonTest extends TestCase {
 
-    public void testSetEintrittsjahr() {
-        int tooLow = 1900;
-        int tooHigh = java.time.LocalDate.now().getYear() + 5;
+    public void testSetEintrittsjahr() throws Exception {
+        int tooLow = 1974;
+        int tooHigh = java.time.LocalDate.now().getYear() + 1;
         int correct = 2000;
+        boolean errorTooHighOccured = false;
+        boolean errorTooLowOccured = false;
 
-        Person p = new Person()
+        Person p = new Person();
 
         try {
-            Person
+            p.setEintrittsjahr(tooLow);
+        } catch (Exception e) {
+            errorTooLowOccured = true;
         }
+        try {
+            p.setEintrittsjahr(tooHigh);
+        } catch (Exception e) {
+            errorTooHighOccured = true;
+        }
+        p.setEintrittsjahr(correct);
 
-        assert
+        assertTrue(errorTooHighOccured);
+        assertTrue(errorTooLowOccured);
+        assertEquals(correct, p.getEintrittsjahr());
+    }
+
+    public void testSetSalaer() throws Exception {
+        double tooLow = -0.01;
+        double tooHigh = 99999.96;
+        double correct = 2000;
+        boolean errorTooHighOccured = false;
+        boolean errorTooLowOccured = false;
+
+        Person p = new Person();
+
+        try {
+            p.setSalaer(tooLow);
+        } catch (Exception e) {
+            errorTooLowOccured = true;
+        }
+        try {
+            p.setSalaer(tooHigh);
+        } catch (Exception e) {
+            errorTooHighOccured = true;
+        }
+        p.setSalaer(correct);
+
+        assertTrue(errorTooHighOccured);
+        assertTrue(errorTooLowOccured);
+        assertEquals(correct, p.getSalaer());
     }
 }
