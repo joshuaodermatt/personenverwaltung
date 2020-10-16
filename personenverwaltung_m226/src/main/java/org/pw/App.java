f6ff65bf16400b71dc3b99b01660c1e8a763f682
joshuaodermatt <joshi.odermatt@gmail.com>
2020-10-16 11:16:45 +0200

    added gui

--- personenverwaltung_m226/src/main/java/org/pw/App.java
+++ personenverwaltung_m226/src/main/java/org/pw/App.java
@@ -6,20 +6,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.w3c.dom.NodeList;
 
 import java.io.File;
-import java.io.FileInputStream;
 
-import static javafx.application.Application.launch;
 
 public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FileInputStream = new File(String.valueOf(getClass().getClassLoader().getResource("main.fxml")));
-        System.out.println(d.isFile());
-        Parent root = FXMLLoader.load(getClass().getResource("resources/main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/mainPage.fxml"));
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
         primaryStage.show();
